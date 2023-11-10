236ae15a5cafefc38f282f4824bdb25d8d295a27
smylinwolf <fabianohaz@gmail.com>
2023-11-10 13:49:13 +0100

    Created Db Migrations

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    tasks = db.relationship('Task', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User(username='{self.username}', email='{self.email}' )"
