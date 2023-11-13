8c98ba6475799aa411c67e38329e8d8d86fac0b8
smylinwolf <fabianohaz@gmail.com>
2023-11-13 09:06:09 +0100

    adjusting tasks

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    tasks = db.relationship('Task', backref='author', lazy=True)
+    tasks = db.relationship('Task', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User(username='{self.username}', email='{self.email}' password='{self.password}' )"
